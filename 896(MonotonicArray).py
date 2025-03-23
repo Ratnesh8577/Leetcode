1cb271e1dfcc4cde4af42cf2f5a58e4a79c1a4bb
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-03-23 22:24:51 +0530

    Add files via upload

---
+++ 896(MonotonicArray).py
@@ -0,0 +1,17 @@
+class Solution:
+    def isMonotonic(self, nums: list[int]) -> bool:
+        increasing = decreasing = True
+        
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                decreasing = False
+            elif nums[i] < nums[i - 1]:
+                increasing = False
+        
+        return increasing or decreasing
+        
+
+
+         
+        
+        
