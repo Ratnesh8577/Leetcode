5673ee9f5e6481e9f8f24c6d8fe2a525338fcbd1
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-04-03 18:11:01 +0530

    Add files via upload

---
+++ 1667(Fix_Names_In_a_Table).py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    return users.assign(
+        name=users['name'].str.capitalize()
+    ).sort_values(by='user_id')
+
+    
