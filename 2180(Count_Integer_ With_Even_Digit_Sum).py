b128ea92cde3e2d2b1c1642e618cd3eda3bcbbb6
Ratnesh Chauhan <ratneshchauhan279@gmail.com>
2025-06-22 18:46:38 +0530

    Add files via upload

---
+++ 2180(Count_Integer_ With_Even_Digit_Sum).py
@@ -0,0 +1,11 @@
+class Solution:
+    def countEven(self, num: int) -> int:
+        def digit_sum(n):
+            return sum(int(d) for d in str(n))
+        
+        count = 0
+        for i in range(1, num + 1):
+            if digit_sum(i) % 2 == 0:
+                count += 1
+        return count
+   
