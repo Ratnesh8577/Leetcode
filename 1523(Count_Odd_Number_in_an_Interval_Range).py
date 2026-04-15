a296fee5e38039ef0931e5bf8c5ece93d0ee4892
Ratnesh Chauhan <ratneshchauhan279@gmail.com>
2026-04-15 23:50:23 +0530

    Update 1523(Count_Odd_Number_in_an_Interval_Range).py

--- 1523(Count_Odd_Number_in_an_Interval_Range).py
+++ 1523(Count_Odd_Number_in_an_Interval_Range).py
@@ -14,6 +14,7 @@ class Solution:
         return (high - low) // 2 + (1 if low % 2 != 0 or high % 2 != 0 else 0)
 
 """
+# Second Method
 
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
